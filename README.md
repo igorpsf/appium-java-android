7992df83788bb35a9be1bb4baf797d3a3806637c
Igor Postnikov <igor.postnikov8@gmail.com>
2020-02-05 14:38:00 -0800

    base.java

---
+++ README.md
@@ -0,0 +1,6 @@
+Jars: 
+1. Java client
+2. Selenium java
+3. Selenium server
+4. Selenium remote driver
+5. TestNG
---
+++ src/main/java/base.java
@@ -0,0 +1,25 @@
+import io.appium.java_client.android.AndroidDriver;
+import io.appium.java_client.android.AndroidElement;
+import io.appium.java_client.remote.MobileCapabilityType;
+import org.openqa.selenium.remote.DesiredCapabilities;
+
+import java.io.File;
+import java.net.MalformedURLException;
+import java.net.URL;
+
+public class base {
+
+    public static void main(String[] args) throws MalformedURLException {
+        // Appium code
+
+        File f = new File("src");
+        File fs = new File(f, "ApiDemos-debug.apk");
+
+        DesiredCapabilities caps = new DesiredCapabilities();
+        caps.setCapability(MobileCapabilityType.DEVICE_NAME, "Igoremulator");
+        caps.setCapability(MobileCapabilityType.AUTOMATION_NAME,"uiautomator2");
+        caps.setCapability(MobileCapabilityType.APP, fs.getAbsolutePath());
+
+        AndroidDriver<AndroidElement> driver = new AndroidDriver(new URL("http://127.0.0.1:4723/wd/hub"), caps);
+    }
+}
