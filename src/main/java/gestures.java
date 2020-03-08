114f5b7fc1c691f8187a0efca5a8b751abb37086
Igor Postnikov <igor.postnikov8@gmail.com>
2020-03-08 14:33:53 -0700

    31

--- src/main/java/gestures.java
+++ src/main/java/gestures.java
@@ -5,16 +5,19 @@ import io.appium.java_client.android.AndroidElement;
 import org.openqa.selenium.WebElement;
 
 import java.net.MalformedURLException;
+import java.util.concurrent.TimeUnit;
 
+import static io.appium.java_client.touch.LongPressOptions.longPressOptions;
 import static io.appium.java_client.touch.TapOptions.tapOptions;
 import static io.appium.java_client.touch.offset.ElementOption.element;
+import static java.time.Duration.ofSeconds;
 
 public class gestures extends base{
 
-    public static void main(String[] args) throws MalformedURLException {
+    public static void main(String[] args) throws MalformedURLException, InterruptedException {
 
         AndroidDriver<AndroidElement> driver = Capabilities();
-
+        driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
         driver.findElementByXPath("//android.widget.TextView[@text='Views']").click();
 
         //Tap
@@ -22,5 +25,12 @@ public class gestures extends base{
         WebElement expandList = driver.findElementByXPath("//android.widget.TextView[@text='Expandable Lists']");
         t.tap(tapOptions().withElement(element(expandList))).perform();
 
+        driver.findElementByXPath("//android.widget.TextView[@text='1. Custom Adapter']").click();
+
+        // Long press
+        WebElement pn = driver.findElementByXPath("//android.widget.TextView[@text='People Names']");
+        t.longPress(longPressOptions().withElement(element(pn)).withDuration(ofSeconds(2))).release().perform();
+        //Thread.sleep(2000);
+        System.out.println(driver.findElementById("android:id/title").isDisplayed());
     }
 }
